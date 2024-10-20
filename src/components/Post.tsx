e1e20618c262f5ce260ec949f7be21d9006c595c
y.shimabukuro <y.shimabukuro@gvc.co.jp>
2024-10-20 22:26:48 +0900

    use variable

---
+++ src/components/Post.tsx
@@ -0,0 +1,15 @@
+const names = ["Maximilian", "Manuel"];
+
+function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
+  return(
+    <div>
+      <p>{chosenName}</p>
+      <p>{2+2}</p>
+      <p>React.js is awesome!</p>
+    </div>
+    );
+}
+
+export default Post;
